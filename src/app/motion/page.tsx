1c5763ca1d938e2cd228e42d17bb4c10d44c98ba
DevONew <kyl2778@naver.com>
2026-04-29 10:11:46 +0900

    motion 실습- 프로그래스바

--- src/app/motion/page.tsx
+++ src/app/motion/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { motion } from "motion/react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react";
 // import * as motion from "motion/react-client"; - 서버 컴포넌트
 
 export default function Home() {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const handleClick = () => {
+    setIsVisible(!isVisible);
+  }
   return (
     <div className="flex h-screen items-center justify-center gap-5">
       <motion.div
@@ -19,11 +25,17 @@ export default function Home() {
       <motion.div
         className="h-24 w-24 rounded-lg border shadow-md"
         initial={{ x: 100 }}
-        animate={{
-            x: 0,
-            transition: {}
-        }}
+        animate={{ x: 0, transition: { duration: 1, type: "spring", bounce: 0.4 } }}
       />
+       <button onClick={handleClick}>Click me</button>
+       <AnimatePresence>
+        {isVisible && (
+            <motion.div
+              className="h-24 w-24 rounded-lg border shadow-md"
+              initial={{ opacity: 0 }}
+            />
+        )}
+       </AnimatePresence>
     </div>
   );
 }
